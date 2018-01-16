1d01ac630456d3b53f24f494b79d15ef22f0c62f
Night3lf <gibslucas@gmail.com>
2018-01-15 21:00:17 -0700

    - Updated sql support

--- src/com/theaudiobros/gabriel/masa/MASA.java
+++ src/com/theaudiobros/gabriel/masa/MASA.java
@@ -1,14 +1,18 @@
 package com.theaudiobros.gabriel.masa;
 
+import com.theaudiobros.gabriel.SQL.MySQLAccess;
 import com.theaudiobros.gabriel.commands.*;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.generator.ChunkGenerator;
 
 import java.io.File;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,6 +43,23 @@ public class MASA extends JavaPlugin{
 
         registerEvents(this, new com.theaudiobros.gabriel.generateworld.JoinGenerateListener(), new com.theaudiobros.gabriel.rocketship.SignCheck(), new com.theaudiobros.gabriel.rocketship.ClickSign(), new com.theaudiobros.gabriel.rocketship.gui.Interact(), new com.theaudiobros.gabriel.SchematicUtil.loadEvent());
 
+        createConfig();
+
+        String mysql_ip = plugin.getConfig().getString("mysql_ip");
+        int mysql_port = plugin.getConfig().getInt("mysql_port");
+        String mysql_database = plugin.getConfig().getString("mysql_database");
+        String mysql_username = plugin.getConfig().getString("mysql_username");
+        String mysql_password = plugin.getConfig().getString("mysql_password");
+
+        MySQLAccess sql = new MySQLAccess();
+        sql.setCredentials(mysql_ip, mysql_port, mysql_database, mysql_username, mysql_password);
+        try {
+            sql.open();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
         boolean loadedSchematics = false;
 
@@ -107,4 +128,24 @@ public class MASA extends JavaPlugin{
 
         }
     }
+
+    private void createConfig() {
+        try {
+            if (!getDataFolder().exists()) {
+                getDataFolder().mkdirs();
+            }
+            File file = new File(getDataFolder(), "config.yml");
+            if (!file.exists()) {
+                getLogger().info("Config.yml not found, creating!");
+                saveDefaultConfig();
+            } else {
+                getLogger().info("Config.yml found, loading!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
 }
